513bba7a6e69db123448ad699a74505e54a8fe73
jiyo01 <jyyy0219@gmail.com>
2023-04-16 19:37:17 +0900

    테스트 코드 작성

---
+++ src/test/java/com/example/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/hello");
+        mvc.perform(builder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+}
