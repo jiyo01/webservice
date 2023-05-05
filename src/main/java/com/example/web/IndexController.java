62abec7c17624c0b75049131bbe987fea7146f17
jiyo01 <jyyy0219@gmail.com>
2023-05-05 18:15:09 +0900

    구글 로그인 어노테이션 기반으로 리팩토링

--- src/main/java/com/example/web/IndexController.java
+++ src/main/java/com/example/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.web;
 
+import com.example.config.auth.LoginUser;
 import com.example.config.auth.dto.SessionUser;
 import com.example.service.posts.PostsService;
 import com.example.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
