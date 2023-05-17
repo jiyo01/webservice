2cdf33430a50afff0eae40a3be198e505f3737b0
jiyo01 <jyyy0219@gmail.com>
2023-05-17 15:58:54 +0900

    세션 저장소를 데이터베이스로 교체

--- src/main/java/com/example/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.example.config.auth.dto;
 import com.example.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
