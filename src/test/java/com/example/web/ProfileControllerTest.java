976933133a428edbd44305f28366287841dc1381
jiyo01 <jyyy0219@gmail.com>
2023-06-29 15:57:07 +0900

    테스트 프로퍼티

--- src/test/java/com/example/web/ProfileControllerTest.java
+++ src/test/java/com/example/web/ProfileControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.web;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +8,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
